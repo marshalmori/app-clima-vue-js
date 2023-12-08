64218d0b4837eee01f42f46b91da80a15fcd7f1e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-08 10:33:04 -0300

    Criar a url para consumir os dados da API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -3,7 +3,10 @@ export default function useClima() {
     // Importar el API Key
     const key = import.meta.env.VITE_API_KEY;
     // Obtener la lat, lng
+
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
     // Obtener el clima
+    console.log(url);
   };
   return { obtenerClima };
 }
