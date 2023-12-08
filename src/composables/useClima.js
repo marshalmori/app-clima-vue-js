a7e43095fdb04cc28254b40106c75ec9d1a20879
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-08 10:59:22 -0300

    Extraindo os valores de latitude e longitude

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,12 +1,21 @@
+import axios from "axios";
+
 export default function useClima() {
-  const obtenerClima = ({ ciudad, pais }) => {
+  const obtenerClima = async ({ ciudad, pais }) => {
     // Importar el API Key
     const key = import.meta.env.VITE_API_KEY;
     // Obtener la lat, lng
+    try {
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+      const { data } = await axios(url);
+      const { lat, lon } = data[0];
 
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+      // Obtener el clima
+    } catch (error) {
+      console.log(error);
+    }
+
     // Obtener el clima
-    console.log(url);
   };
   return { obtenerClima };
 }
